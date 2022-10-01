9c6260bde95294fd086052b797d65607c4f34ca6
Martijn Schrage <martijn@oblomov.com>
2022-10-01 19:02:53 +0200

    Configure prettier + Format all sources

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+module.exports = {
+  $schema: 'http://json.schemastore.org/prettierrc',
+  arrowParens: 'always',
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>
 }
 
-export default App;
+export default App
